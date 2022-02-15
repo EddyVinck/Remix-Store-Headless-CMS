a027b6e08625996b0233fca16146499fef0f15d9
Eddy Vinck <eddy.vinck@isaac.nl>
2022-02-15 13:50:55 +0100

    feat: move the slicemachine to the homepage, remove test page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,38 @@
+import { json, LoaderFunction, useLoaderData } from "remix";
+import { prismicClient } from "~/utils/prismic.server";
+import { SliceLike, SliceZone } from "@prismicio/react";
+import HeroSlice from "../../slices/HeroSlice";
+import { SliceZoneContext } from "~/types/prismic";
+// import {} from "@prismicio/helpers";
+
+type LoaderData = { slices: SliceLike[]; doc: unknown };
+
+export const loader: LoaderFunction = async () => {
+  try {
+    const doc = await prismicClient.getByUID("homepage", "home", {});
+    const data: LoaderData = { slices: doc.data.slices, doc };
+    return json(data);
+  } catch (error) {
+    throw new Response("What a joke! Not found.", {
+      status: 404,
+    });
+  }
+};
+
 export default function Index() {
+  const data = useLoaderData<LoaderData>();
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <p>Loader data:</p>
+      <SliceZone<any, SliceZoneContext>
+        slices={data.slices}
+        components={{
+          hero_slice: HeroSlice,
+        }}
+      />
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
 }
