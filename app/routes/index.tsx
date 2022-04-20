6b7beba9046c344ac0e4fa10c83aecd7e591a2c0
Eddy Vinck <eddy.vinck@isaac.nl>
2022-04-20 16:40:55 +0200

    feat: add book categories

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -37,6 +37,8 @@ export const loader: LoaderFunction = async () => {
 
 export default function Index() {
   const data = useLoaderData<LoaderData>();
+
+  console.log(data.books);
   return (
     <main>
       <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
