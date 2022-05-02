c4bc14cd3c1e21329488b73537f6d5681a410a62
Eddy Vinck <eddy.vinck@isaac.nl>
2022-05-02 14:44:12 +0200

    feat(search): show categories, use layout route

--- app/routes/search/$searchString.tsx
+++ app/routes/search/$searchString.tsx
@@ -1,40 +1,14 @@
 import { predicate } from "@prismicio/client";
-import {
-  ActionFunction,
-  LoaderFunction,
-  useActionData,
-  useLoaderData,
-} from "remix";
+import { LoaderFunction, useLoaderData, useParams } from "remix";
 import { BookListItem } from "~/components/book";
 import { Link } from "~/components/link";
 import { PrismicDocument } from "~/types/prismic";
 import { prismicClient } from "~/utils/prismic.server";
 import { BookList, bookListDataQuery } from "~/utils/queries/book-list-query";
 
-export const action: ActionFunction = async ({ request, params }) => {
-  // const { searchString = "" } = params;
-
-  // if (!searchString) {
-  //   throw new Response("Not found.", {
-  //     status: 404,
-  //   });
-  // }
-
-  // return {
-  //   searchString,
-  // };
-
-  // redirect('/search/' + searchString);
-  return {
-    ok: true,
-    request,
-    params,
-  };
-};
-
 type LoaderData = { books: BookList; bookCategory: PrismicDocument };
 
-export const loader: LoaderFunction = async ({ request, params }) => {
+export const loader: LoaderFunction = async ({ params }) => {
   const { searchString = "" } = params;
 
   if (!searchString) {
@@ -60,13 +34,15 @@ export const loader: LoaderFunction = async ({ request, params }) => {
 };
 
 export default function SearchResults() {
-  const stuff = useActionData();
   const data = useLoaderData<LoaderData>();
+  const params = useParams();
 
   return (
     <section className="container py-16">
       <div>
-        <h2 className="text-4xl font-bold leading-tight mb-6">Categories</h2>
+        <h2 className="text-4xl font-bold leading-tight mb-6">
+          Results for "{params.searchString}"
+        </h2>
         <div>
           <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 gap-y-10">
             {data.books.map((book) => (
